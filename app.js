6f59058496cf30911e4ce13a57d6ba533a1ecfd4
TheMole3 <hugomelo04@gmail.com>
2020-12-05 15:34:58 +0100

    Added express

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const chrome = require('selenium-webdriver/chrome');
 const fs = require("fs")
 const path = require('path');
 
+const express = require('express');
+const app = express();
+
 function checkExistsWithTimeout(filePath, timeout) {
     return new Promise(function (resolve, reject) {
 
@@ -60,3 +63,8 @@ options.setUserPreferences(
     await driver.quit();
   }
 })();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`listening to port ${port} now...`);
+});
